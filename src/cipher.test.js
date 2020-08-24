7b41cafa5d8f7f8f5351936e1bd3831c4ed0cb9a
keshav-c <keshav1985@gmail.com>
2020-08-24 19:46:14 +0530

    Add tests for ceasar cipher

---
+++ src/cipher.test.js
@@ -0,0 +1,16 @@
+const cipher = require('./cipher');
+
+it('Encrypts string', () => {
+  expect(cipher('defend the east wall of the castle', 1))
+    .toBe('efgfoe uif fbtu xbmm pg uif dbtumf');
+});
+
+it('Encrypts string', () => {
+  expect(cipher('Microverse', 3))
+    .toBe('Plfuryhuvh');
+});
+
+it('Encrypts string', () => {
+  expect(cipher('y', 5))
+    .toBe('d');
+});
